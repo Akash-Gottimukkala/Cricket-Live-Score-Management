05d3a354d03cf64557582b6a71c8d7db301ddbcc
Akash-Gottimukkala <76110994+Akash-Gottimukkala@users.noreply.github.com>
2023-03-23 15:38:35 +0530

    Update index.php

--- cricinfo/index.php
+++ cricinfo/index.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 	<title> Live Scores &amp; Updates </title>
-	<link   rel="stylesheet" href="css/style.css" type="text/css"> </link>
+	<link   rel="stylesheet" href="style.css" type="text/css"> </link>
 </head>
 <body>
 <div>
 	<header>
-		<h1>Live Scores</h1>
+		<h1>GPL Live Scores</h1>
 	</header>
 	<nav>
 	   <ul>
@@ -23,7 +23,7 @@
 		
 	</section>
 	<footer>
-		<p>copyright &copy; Md. Moniruzzaman Aurangzeb</p>
+		<p>copyright &copy;Gottimukkala Akash</p>
 	</footer>
 </body>
 </html>
