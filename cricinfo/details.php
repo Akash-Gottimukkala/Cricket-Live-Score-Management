5a7000d3b557de51776d321614841dca5d9f5fa3
Akash-Gottimukkala <76110994+Akash-Gottimukkala@users.noreply.github.com>
2023-03-23 15:39:36 +0530

    Update details.php

--- cricinfo/details.php
+++ cricinfo/details.php
@@ -79,13 +79,13 @@
 <head>
 	<meta http-equiv="refresh" content="5" />
 	<title> Live Scores </title>
-	<link   rel="stylesheet" href="css/style.css" type="text/css"> </link>
+	<link   rel="stylesheet" href="style.css" type="text/css"> </link>
 	
 </head>
 <body>
 <div>
 	<header>
-		<h1>Live Scores</h1>
+		<h1>GPL Live Scores</h1>
 	</header>
 	<nav>
 	   <ul>
